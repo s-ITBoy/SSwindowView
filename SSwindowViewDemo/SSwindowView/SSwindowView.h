d6d4cd904cd8e4a3f84eba3157e0dcdfd92b9101
FL S <>
2023-10-08 12:21:41 +0800

    这个很早之前就做过优化了，一直没更新上去，现在把它放上去。

--- SSwindowViewDemo/SSwindowView/SSwindowView.h
+++ SSwindowViewDemo/SSwindowView/SSwindowView.h
@@ -8,34 +8,86 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, SSContentPosition) {
+    SSContentPositionCenter   = 0,
+    SSContentPositionTop,
+    SSContentPositionLeft,
+    SSContentPositionBottom,
+    SSContentPositionRight,
+};
+
 NS_ASSUME_NONNULL_BEGIN
 ///弹框视图基类
 @interface SSwindowView : UIView
 
-#pragma mark -------- 使用方式一 --------------
-
-@property (strong,nonatomic,readonly) UIView* backgroundView;
-@property (weak,nonatomic) UIView* contentView;
-@property (assign,nonatomic) BOOL anim;
-@property (assign,nonatomic) BOOL isShow;
-@property (assign,nonatomic) UIViewContentMode contentMode;
+#pragma mark -------- 使用方式一 废弃 --------------
 -(instancetype) customViewWithAlpha:(CGFloat) alp;
 -(instancetype) customerView;
--(void) show;
--(void) dismiss;
+-(void) show:(BOOL)animate;
+-(void) dismiss:(BOOL)animate;
 
-#pragma mark -------- 使用方式二（推荐使用） --------------
+#pragma mark -------- 使用方式二 添加到keywindow上的弹框（推荐使用） --------------
+/**
+ *弹框展示（带全属性）
+ *@param contentView 需要展示的内容视图
+ *@param contentPosition contentView展示的位置
+ *@param offset 偏移量；当positon = center / top / bottom 时 offset为y轴的偏移量； 当position = left / right 时 offset为x轴偏移量
+ *@param isCan 空白区域能否点击
+ *@param bgAlpha 背景透明度值
+ *param showAnimate 是否显示弹出动画 此属性默认为YES
+ */
++ (void)ss_show:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan bgAlpha:(CGFloat)bgAlpha animate:(BOOL)showAnimate;
+/**
+ *弹框展示（默认偏移量offset == 0；空白区域能点击，bgalpha==0.65）
+ *@param contentView 需要展示的内容视图
+ *@param contentPosition contentView展示的位置 \n
+ */
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition;
+/**
+ *弹框展示（默认空白区域能点击 bgalpha==0.65）
+ *@param contentView 需要展示的内容视图
+ *@param contentPosition contentView展示的位置
+ *@param offset 偏移量；当positon = center / top / bottom 时 offset为y轴的偏移量； 当position = left / right 时 offset为x轴偏移量
+ */
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset;
+/**
+ *弹框展示 (默认 bgalpha==0.65)
+ *@param contentView 需要展示的内容视图
+ *@param contentPosition contentView展示的位置
+ *@param offset 偏移量；当positon = center / top / bottom 时 offset为y轴的偏移量； 当position = left / right 时 offset为x轴偏移量
+ *@param isCan 空白区域能否点击
+ */
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan;
 
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan complet:(void(^)(void))complete;
+
++ (BOOL)ss_isShowCustomView;
+///弹框关闭
++ (void)ss_dismissCustomView:(BOOL)animate;
+///一次性关闭多个弹框
++ (void)ss_dismissAllCustomView;
+
+#pragma mark -------- 使用方式三 添加到指定superView上的弹框（推荐使用） --------------
 /**
  *弹框展示
- *contentView: 需要展示的内容
- *contentMode:  contentView展示的位置(可取值为：UIViewContentModeCenter | UIViewContentModeTop | UIViewContentModeBottom | UIViewContentModeLeft | UIViewContentModeRight,)
+ *@param contentView 需要展示的内容视图
+ *@param superView 弹框将要展示的位置的视图层
+ *@param contentPosition contentView展示的位置
+ *@param offset 偏移量；当positon = center / top / bottom 时 offset为y轴的偏移量； 当position = left / right 时 offset为x轴偏移量
+ *@param isCan 空白区域能否点击
+ *@param bgAlpha 背景透明度值
+ *@param showAnimate 是否显示弹出动画
  */
-+ (void)showView:(UIView*)contentView contentMode:(UIViewContentMode)contentMode;
-
-+ (BOOL)isShowCustomView;
-///弹框关闭
-+ (void)dismissCustomView;
++ (void)ss_showView:(UIView*)contentView superView:(UIView*)superView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan bgAlpha:(CGFloat)bgAlpha animate:(BOOL)showAnimate;
+/**
+ *弹框展示 (默认 bgalpha==0.65 showAnimate==YES)
+ *@param contentView 需要展示的内容视图
+ *@param superView 弹框将要展示的位置的视图层
+ *@param contentPosition contentView展示的位置
+ *@param offset 偏移量；当positon = center / top / bottom 时 offset为y轴的偏移量； 当position = left / right 时 offset为x轴偏移量
+ *@param isCan 空白区域能否点击
+ */
++ (void)ss_showView:(UIView*)contentView superView:(UIView*)superView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan;
 @end
 
 NS_ASSUME_NONNULL_END
--- SSwindowViewDemo/SSwindowView/SSwindowView.m
+++ SSwindowViewDemo/SSwindowView/SSwindowView.m
@@ -6,10 +6,28 @@
 //  Copyright © 2017 F S. All rights reserved.
 //
 
-#define is_iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+//#define is_iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
 
 #import "SSwindowView.h"
 
+@interface UIView (superView)
+
+@end
+@implementation UIView (superView)
+
+@end
+@interface SSwindowView ()
+@property (nonatomic,strong,readonly) UIView* backgroundView;
+@property (nonatomic,weak) UIView* contentView;
+@property (nonatomic,assign) BOOL anim;
+@property (nonatomic,assign) BOOL isShow;
+@property (nonatomic,assign) SSContentPosition contentPosition;
+///偏移量
+@property(nonatomic,assign) CGFloat offset;
+///空白区域能搜点击
+@property (nonatomic,assign) BOOL isCanClickEmpt;
+
+@end
 @implementation SSwindowView {
     CGRect _beforeFrame;
     CGRect _showFrame;
@@ -17,53 +35,137 @@
     CGFloat _yyy;
 }
 
--(instancetype)customViewWithAlpha:(CGFloat) alp {
+- (instancetype)customViewWithAlpha:(CGFloat) alp {
     if([super initWithFrame:CGRectMake(0.0f, 0.0f, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)]){
         _alpha = alp;
         _backgroundView = [[UIView alloc]initWithFrame:self.frame];
         _backgroundView.backgroundColor = [UIColor blackColor];
-        [_backgroundView addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(dismiss)]];
+        [_backgroundView addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickDismiss)]];
         [self addSubview:_backgroundView];
-        _contentMode = UIViewContentModeCenter;
+        _contentPosition = SSContentPositionCenter;
         self.tag = 999;
     }
     return self;
 }
--(instancetype)customerView {
+- (instancetype)customerView {
     [self customViewWithAlpha:0.65];
     return self;
 }
 
--(void)setContentMode:(UIViewContentMode)contentMode {
-    _contentMode = contentMode;
+- (void)setContentPosition:(SSContentPosition)contentPosition {
+    _contentPosition = contentPosition;
     CGSize size = _contentView.frame.size;
-    CGFloat offset = is_iPhoneX ? 34 : 0;
-    switch (_contentMode) {
-        case UIViewContentModeTop:
-            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , 0.0f, size.width, size.height);
+    switch (contentPosition) {
+        case SSContentPositionTop:
+            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , 0.0f + self.offset, size.width, size.height);
             _beforeFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , -size.height, size.width, size.height);
             break;
-        case UIViewContentModeBottom:
-            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , self.frame.size.height-size.height - offset, size.width, size.height + offset);
+        case SSContentPositionBottom: {
+//            CGFloat bottomSafeArea = isIphoneX ? 34 : 0;
+//            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , self.frame.size.height-size.height - bottomSafeArea + self.offset, size.width, size.height + bottomSafeArea);
+            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , self.frame.size.height-size.height + self.offset, size.width, size.height);
             _beforeFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , self.frame.size.height+size.height, size.width, size.height);
+        }
             break;
-        case UIViewContentModeLeft:
-            _showFrame = CGRectMake(0, (self.frame.size.height-size.height)/2, size.width, size.height);
+        case SSContentPositionLeft:
+            _showFrame = CGRectMake(0 + self.offset, (self.frame.size.height-size.height)/2, size.width, size.height);
             _beforeFrame = CGRectMake(-size.width, (self.frame.size.height-size.height)/2, size.width, size.height);
             break;
-        case UIViewContentModeRight:
-            _showFrame = CGRectMake(self.frame.size.width - size.width , (self.frame.size.height-size.height)/2, size.width, size.height);
+        case SSContentPositionRight:
+            _showFrame = CGRectMake(self.frame.size.width - size.width + self.offset , (self.frame.size.height-size.height)/2, size.width, size.height);
             _beforeFrame = CGRectMake( self.frame.size.width +size.width , (self.frame.size.height-size.height)/2, size.width, size.height);
             break;
-        case UIViewContentModeCenter:
-        default:
-            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , (self.frame.size.height-size.height)/2.0f, size.width, size.height);
+        case SSContentPositionCenter:
+//            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , (self.frame.size.height-size.height)/2.0f, size.width, size.height);
+            _showFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , (self.frame.size.height-size.height)/2.0f + self.offset, size.width, size.height);
             _beforeFrame = CGRectMake( (self.frame.size.width - size.width)/2.0f , -size.height, size.width, size.height);
+            break;
+            
+        default:
+            
             break;
     }
 }
 
--(void)show {
+- (void)show:(BOOL)animate {
+    [self addSubview:_contentView];
+    [[UIApplication sharedApplication].keyWindow addSubview:self];
+    _contentView.frame = _beforeFrame;
+    _backgroundView.alpha = 0.0f;
+    if (animate) {
+        [UIView animateWithDuration:0.35f animations:^{
+            self.backgroundView.alpha = self->_alpha;
+            self.contentView.frame = self->_showFrame;
+        }];
+    } else {
+        self.backgroundView.alpha = _alpha;
+        self.contentView.frame = _showFrame;
+    }
+    
+    _isShow = YES;
+    
+}
+
+- (void)clickDismiss {
+    if (self.isCanClickEmpt) {
+        [self dismiss:YES];
+    }
+}
+
+- (void)dismiss:(BOOL)animate {
+    if (animate) {
+        [UIView animateWithDuration:0.35 animations:^{
+            self->_backgroundView.alpha = 0.0f;
+            self->_contentView.frame = self->_beforeFrame;
+        } completion:^(BOOL finished) {
+            self.contentView = nil;
+            [self.contentView removeFromSuperview];
+            [self removeFromSuperview];
+        }];
+        
+    } else {
+        _backgroundView.alpha = 0.0f;
+        _contentView.frame = _beforeFrame;
+        [self.contentView removeFromSuperview];
+        self.contentView = nil;
+        [self removeFromSuperview];
+    }
+    _isShow = NO;
+}
+
+- (void)ss_setSub:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan bgAlpha:(CGFloat)bgAlpha {
+    self.contentView = contentView;
+    self.contentPosition = contentPosition;
+    self.offset = offset;
+    self.isCanClickEmpt = isCan;
+    _alpha = bgAlpha;
+}
+
++ (void)ss_show:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan bgAlpha:(CGFloat)bgAlpha animate:(BOOL)showAnimate {
+    SSwindowView* view = [[SSwindowView alloc] customerView];
+    [view ss_setSub:contentView position:contentPosition offset:offset ClickEmpt:isCan bgAlpha:bgAlpha];
+    [view show:showAnimate];
+}
+
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition {
+    [self ss_show:contentView position:contentPosition offset:0 ClickEmpt:YES bgAlpha:0.65 animate:YES];
+}
+
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset {
+    [self ss_show:contentView position:contentPosition offset:offset ClickEmpt:YES bgAlpha:0.65 animate:YES];
+}
+
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan {
+    [self ss_show:contentView position:contentPosition offset:offset ClickEmpt:isCan bgAlpha:0.65 animate:YES];
+}
+
++ (void)ss_showView:(UIView*)contentView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan complet:(void(^)(void))complete {
+    SSwindowView* view = [[SSwindowView alloc] customerView];
+    [view ss_setSub:contentView position:contentPosition offset:offset ClickEmpt:isCan bgAlpha:0.65];
+    [view showWithBlock:complete];
+}
+
+- (void)showWithBlock:(void(^)(void))complete {
     [self addSubview:_contentView];
     [[UIApplication sharedApplication].keyWindow addSubview:self];
     _contentView.frame = _beforeFrame;
@@ -71,40 +173,76 @@
     [UIView animateWithDuration:0.35f animations:^{
         self.backgroundView.alpha = self->_alpha;
         self.contentView.frame = self->_showFrame;
+    } completion:^(BOOL finished) {
+        if (finished) {
+            complete();
+        }
     }];
     _isShow = YES;
     
 }
--(void)dismiss {
-    [UIView animateWithDuration:0.35 animations:^{
-        self->_backgroundView.alpha = 0.0f;
-        self->_contentView.frame = self->_beforeFrame;
-    } completion:^(BOOL finished) {
-        self.contentView = nil;
-        [self.contentView removeFromSuperview];
-        [self removeFromSuperview];
-    }];
-    _isShow = NO;
-}
 
-+ (void)showView:(UIView*)contentView contentMode:(UIViewContentMode)contentMode {
-    SSwindowView* view = [[SSwindowView alloc] customerView];
-    view.contentView = contentView;
-    view.contentMode = contentMode;
-    [view show];
-//    return view;
-    
-}
-
-+ (BOOL)isShowCustomView {
++ (BOOL)ss_isShowCustomView {
     SSwindowView* view = [[UIApplication sharedApplication].keyWindow viewWithTag:999];
     return view != nil;
 }
 
-+ (void)dismissCustomView {
++ (void)ss_dismissCustomView:(BOOL)animate {
     SSwindowView* view = [[UIApplication sharedApplication].keyWindow viewWithTag:999];
     if(view)
-        [view dismiss];
+        [view dismiss:animate];
+}
+
++ (void)ss_dismissAllCustomView {
+    for (UIView* view in [UIApplication sharedApplication].keyWindow.subviews) {
+        if (view.tag == 999) {
+            SSwindowView* w = (SSwindowView*)view;
+            [w dismiss:YES];
+        }
+    }
+}
+
+#pragma mark -------- 使用方式三 添加到指定superView上的弹框（推荐使用） --------------
+
++ (void)ss_showView:(UIView*)contentView superView:(UIView*)superView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan bgAlpha:(CGFloat)bgAlpha animate:(BOOL)showAnimate {
+    SSwindowView* view = [[SSwindowView alloc] initWithSuper:superView];
+    [view ss_setSub:contentView position:contentPosition offset:offset ClickEmpt:isCan bgAlpha:bgAlpha];
+    [view showWithSuper:superView animate:showAnimate];
+}
+
++ (void)ss_showView:(UIView*)contentView superView:(UIView*)superView position:(SSContentPosition)contentPosition offset:(CGFloat)offset ClickEmpt:(BOOL)isCan {
+    [self ss_showView:contentView superView:superView position:contentPosition offset:offset ClickEmpt:isCan bgAlpha:0.65 animate:YES];
+}
+
+- (void)showWithSuper:(UIView*)superView animate:(BOOL)showAnimate {
+    [self addSubview:_contentView];
+    [superView addSubview:self];
+    _contentView.frame = _beforeFrame;
+    _backgroundView.alpha = 0.0f;
+    if (showAnimate) {
+        [UIView animateWithDuration:0.35f animations:^{
+            self.backgroundView.alpha = self->_alpha;
+            self.contentView.frame = self->_showFrame;
+        }];
+    } else {
+        self.backgroundView.alpha = _alpha;
+        self.contentView.frame = _showFrame;
+    }
+    
+    _isShow = YES;
+}
+
+- (instancetype)initWithSuper:(UIView*)superView {
+    if ([super initWithFrame:superView.bounds]) {
+        _alpha = 0.65;
+        _backgroundView = [[UIView alloc]initWithFrame:self.frame];
+        _backgroundView.backgroundColor = [UIColor blackColor];
+        [_backgroundView addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickDismiss)]];
+        [self addSubview:_backgroundView];
+        _contentPosition = SSContentPositionCenter;
+        self.tag = 999;
+    }
+    return self;
 }
 
 /*
